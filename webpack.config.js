9ec22c7193563c526b2f739f77bbdccc02a5d01a
Adam Anderly <adam@anderly.com>
2018-08-15 16:32:22 -0500

    Cleanup and use babel-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,21 @@ require('dotenv').config({ path: __dirname + '/cloudflare.env' });
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: __dirname + '/src/index.js',
     output: {
         path: __dirname + '/dist',
         filename: 'index.js',
     },
     target: 'webworker',
     mode: 'production',
-    optimization: {
-        minimize: false,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: [/frequency_lists/],
+            },
+        ],
     },
     performance: {
         hints: false,
